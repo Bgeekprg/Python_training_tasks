3d752e6aafa08c5890a0ab30aa463d6c796829f4
Bgeekprg <bhaveshpr54@gmail.com>
2025-01-23 11:49:31 +0530

    refactor:changed program as per description

--- DiceGame.py
+++ DiceGame.py
@@ -1,35 +1,16 @@
 import random
 
-
 def roll_dice():
     return random.randint(1, 6)
 
-
-def start_game():
-    print("-------- Dice Game -------")
-    input("Press Enter to roll the dice...(Player 1)")
-
-    player1_roll = roll_dice()
-    print(f"player1 rolled a {player1_roll}!\n")
-    
-    input("Press Enter to roll the dice...(Player 2)")
-    player2_roll = roll_dice()
-    print(f"player2 rolled a {player2_roll}!\n")
-
-    print("----------------------------")
-    if player1_roll > player2_roll:
-        print("player1 win!")
-    elif player1_roll < player2_roll:
-        print("player2 wins!")
+while True:
+    user_input = input("Press 1 to roll the dice or 0 to exit: ")
+        
+    if user_input == '1':
+        dice_roll = roll_dice()
+        print(f"The dice rolled: {dice_roll}")
+    elif user_input == '0':
+        print("Exiting the dice roll program.")
+        break
     else:
-        print("It's a tie!")
-
-
-
-
-start_game()
-choice=input("\n You want to play Again (y/Y) ")
-while(choice.upper()=="Y"):
-    start_game()
-    choice=input("\n You want to play Again (y/Y) ")
-
+        print("Invalid input. Please press 1 to roll the dice or 0 to exit.")
